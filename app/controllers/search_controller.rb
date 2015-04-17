7a592b741c9085411d4daf1971da8876f3fe6458
github username <github email address>
2015-04-17 11:25:08 -0400

    working on results show page...

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,17 @@
 class SearchController < ApplicationController
-  def index
-    @results = User.where(name: @result.name.include?(params[:keyword]))
+  # def index
+  #   binding.pry
+  #   @results = User.where(name: @result.name.include?(params[:keyword]))
 
-    if @result
-      # redirect_to
-    # elsif !Search.for(params[:keyword]).empty?
-      @results = Search.for(params[:keyword])
-    else
-      flash[:notice] = "No results match your search..."
-      # redirect_to root_path
-    end
-  end
+  #   if @result
+  #     # redirect_to
+  #   # elsif !Search.for(params[:keyword]).empty?
+  #     @results = Search.for(params[:keyword])
+  #   else
+  #     flash[:notice] = "No results match your search..."
+  #     # redirect_to root_path
+  #   end
+  # end
 
   # def show
   # end
@@ -19,8 +20,9 @@ class SearchController < ApplicationController
   # end
 
   def create
-    # binding.pry
     @results = Search.for(params["keyword"])
+    @users = User.where('name like ? OR email like ?', params['keyword'], params['keyword'])
+
     # if @results.empty?
     #   flash[:notice] = "No results match your search..."
     #   # redirect_to root_path
@@ -28,6 +30,8 @@ class SearchController < ApplicationController
     @group = Group.find(params[:group_id].to_i)
     @invitation = Invitation.new
       # @user_group = UserGroup.new
+    @users = @results[0]
+    @groups = @results[1..-1]
     render :results
     # end
   end
