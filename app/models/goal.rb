088acfc49248fd91587ae44f68c7b9157aa617ab
cyzanfar <cghazanfar10@gmail.com>
2015-04-17 13:35:44 -0400

    refactor

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,8 +1,6 @@
 class Goal < ActiveRecord::Base
 
 	has_many :activities, dependent: :destroy
-  # has_many :goal_tags
-  # has_many :tags, through: :goal_tags
   has_many :comments, as: :commentable, dependent: :destroy
   belongs_to :user
   validates :name, :description, presence: true
@@ -21,30 +19,6 @@ class Goal < ActiveRecord::Base
 
   def days_left
     (self.due_date - Time.now.to_date).to_i
-  end
-
-
-  def total_activity_occurence
-    occurence = 0
-    self.activities.each do |activity|
-      if activity.number_occurences
-        occurence += activity.number_occurences
-      end
-    end
-    occurence
-  end
-
-  def total_remaining_occurence
-    remaining = 0
-    self.activities.each do |activity|
-      remaining += activity.occurences
-    end
-    remaining
-  end
-
-
-  def percentage_activity_occurence_complete
-    ((1 - (self.total_remaining_occurence / self.total_activity_occurence.to_f)) * 100).round(1)
   end
 
 end
