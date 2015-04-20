041a3ad53b6eb8a6df656c9c2ffcdec98cec453d
github username <github email address>
2015-04-20 14:05:16 -0400

    reincorporated sidebar on theme_header and commented out some assets not in use

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
   has_many :cheers
   # has_many :groups, through: :user_groups
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: "/:style/missing_avatar.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   validates :name, presence: true
